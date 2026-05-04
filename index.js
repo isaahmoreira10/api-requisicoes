fe85a136dfa362212b49229af6c2040ed75c1d0a
GuriDoSul22 <barbosa.moreira.isabella>
2026-05-04 12:32:31 -0300

    .....

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/saudacao",(req, res) => {
+})
+
+app.post("/imc",(req, res) =>{
+    const { nome, idade, altura, peso }=req.body;
+
+
+    if (!nome || !idade || !altura || !peso) {
+        return res,status(404).json({erro: "Dados incompletos"})
+    }
+
+    const imc = peso / (altura * altura);
+    res.json({
+        nome,
+        idade,
+        imc: imc.toFixed(2)
+    })
+
+
+}
+
+
+app.listen(port,() => {
+    console.log(`Servidor rodando em http://localhost:${port}`)
+
+})
